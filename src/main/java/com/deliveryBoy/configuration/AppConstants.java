36a52c683ff0c7630db9b6b10b1a71f6ed7bcaad
jetpu <jetpuriya96@gmail.com>
2025-01-07 18:01:54 +0530

    [P-07-01-25] DELIVERYBOY

---
+++ src/main/java/com/deliveryBoy/configuration/AppConstants.java
@@ -0,0 +1,7 @@
+package com.deliveryBoy.configuration;
+
+public class AppConstants {
+//    public static final String LOCATION_TOPIC_NAME = "location-update-topic";
+    public static final String LOCATION = "location-update";
+
+}
